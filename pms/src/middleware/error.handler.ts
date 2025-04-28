6ad8c91a68e9901fcff61beb1d4ec3c0210317c9
dev-Bambam <ayogood18@gmail.com>
2025-04-28 19:34:14 +0100

    will continue tomorrow, I am planning of using OOP in building services

--- pms/src/middleware/error.handler.ts
+++ pms/src/middleware/error.handler.ts
@@ -1,19 +1,22 @@
-import { CustomError } from "utils/Error/Custom.error";
-import { Request, Response, NextFunction } from 'express'
+import { CustomError, ValidationError } from "utils/Error/Custom.error";
+import { Request, Response, NextFunction } from "express";
 
 const errorHandler = (err: CustomError, _req: Request, res: Response, next: NextFunction) => {
-    const status = err.status ?? 'fail';
-    const statusCode = err.statusCode ?? 500;
-    const type = err.type ?? "SERVER_ERR";
-    const message = err.message;
-    const meta = {};
+   const status = err.status ?? "fail";
+   const statusCode = err.statusCode ?? 500;
+   const type = err.type ?? "SERVER_ERR";
+   const message = err.message;
+   let meta = {};
 
-    res.status(statusCode).json({
-        status,
-        type,
-        message,
-        ...meta
-    })
-}
+   if (err instanceof ValidationError) {
+      meta = { details: err.details };
+   }
+   res.status(statusCode).json({
+      status,
+      type,
+      message,
+      ...meta,
+   });
+};
 
-export default errorHandler
+export default errorHandler;
