57411ac6db60e0bfdbefd646253be631e29f8706
dev-Bambam <ayogood18@gmail.com>
2025-05-04 23:00:37 +0100

    trying to solidify my knowledge in OOP fundamentals in TS

--- pms/oop/test.ts
+++ pms/oop/test.ts
@@ -1,13 +1,24 @@
-const animal = {
-    speak() {
-        console.log('Animal speak')
-    }
+
+interface Todo{
+    title: string;
+    description?:string
 }
 
-const dog = Object.create(animal)
-dog.bark = function () {
-    console.log("dog barks")
+const todo1 = {
+    title: "I will learn SOLID design principles",
+    completed: false
 }
 
-dog.speak()
-dog.bark()
+const updateTodo = (todo: Todo, fieldsToUpdate: Partial<Todo>) => ({ ...todo, ...fieldsToUpdate })
+
+const result1 = updateTodo(todo1, { description: "throw out trash" });
+
+const todo2 = {
+    ...todo1,
+    description: "clean up the house thoroughly"
+}
+
+const updateRequiredTodo = (todo: Required<Todo>, fieldsToUpdate: Partial<Todo>) => ({ ...todo, ...fieldsToUpdate });
+
+const result2 = updateRequiredTodo(todo2, { description: "throw out trash" })
+console.log(`Result1:${result1}`, "\n", `Result2:${result2}`)
