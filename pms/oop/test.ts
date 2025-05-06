dc4a20e529e663ec7723445f2a64d584ffa412a8
dev-Bambam <ayogood18@gmail.com>
2025-05-06 17:11:57 +0100

    have started learning OOP principles

--- pms/oop/test.ts
+++ pms/oop/test.ts
@@ -1,24 +1,6 @@
-
-interface Todo{
-    title: string;
-    description?:string
+class User{
+    constructor(
+        public name: string,
+        public email: string
+    ){}
 }
-
-const todo1 = {
-    title: "I will learn SOLID design principles",
-    completed: false
-}
-
-const updateTodo = (todo: Todo, fieldsToUpdate: Partial<Todo>) => ({ ...todo, ...fieldsToUpdate })
-
-const result1 = updateTodo(todo1, { description: "throw out trash" });
-
-const todo2 = {
-    ...todo1,
-    description: "clean up the house thoroughly"
-}
-
-const updateRequiredTodo = (todo: Required<Todo>, fieldsToUpdate: Partial<Todo>) => ({ ...todo, ...fieldsToUpdate });
-
-const result2 = updateRequiredTodo(todo2, { description: "throw out trash" })
-console.log(`Result1:${result1}`, "\n", `Result2:${result2}`)
