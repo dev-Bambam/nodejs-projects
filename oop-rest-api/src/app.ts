ad73eef9b9a62f2e098b53d92d6d1dcb6d9771a1
dev-Bambam <ayogood18@gmail.com>
2025-05-14 23:38:19 +0100

    debugging mode activated....Lol..But I am feeling sick for it

--- oop-rest-api/src/app.ts
+++ oop-rest-api/src/app.ts
@@ -1,46 +1,46 @@
-import express, { Application } from 'express'
-import cors from "cors"
-import helmet from 'helmet'
-import ErrorHandler from './helpers/error.handler'
-import Database from './config/db.config'
-import userRoute from './routes/user.route'
+import express, { Application } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import ErrorHandler from "./helpers/error.handler";
+import Database from "./config/db.config";
+import userRoute from "./routes/user.route";
 
-class App{
-    private readonly app: Application
-    private readonly port: number
-    
-    constructor() {
-        this.app = express()
-        this.port = parseInt(process.env.PORT || "3000")
-        this.init()
-    }
-    private init() {
-        this.initConfig()
-        this.initMiddleWares()
-        this.initRoutes()
-        this.initErrorHandling()
-    }
-    private initConfig() {
-        new Database()
-    }
-    private initMiddleWares() {
-        this.app.use(helmet())
-        this.app.use(cors())
-        this.app.use(express.json())
-        this.app.use(express.urlencoded({extended:true}))
-    }
-    private initRoutes(){
-        this.app.use('api/v1/users', userRoute)
-    }
-    private initErrorHandling() {
-        this.app.use(ErrorHandler.NotFound);
-        this.app.use(ErrorHandler.serverError);
-    }
-    public listen() {
-        this.app.listen(this.port, () => {
-            console.log(`Server running at localhost:${this.port}`)
-        })
-    }
+class App {
+   private readonly app: Application;
+   private readonly port: number;
+
+   constructor() {
+      this.app = express();
+      this.port = parseInt(process.env.PORT || "3000");
+      this.init();
+   }
+   private init() {
+      this.initConfig();
+      this.initMiddleWares();
+      this.initRoutes();
+      this.initErrorHandling();
+   }
+   private initConfig() {
+      new Database();
+   }
+   private initMiddleWares() {
+      this.app.use(helmet());
+      this.app.use(cors());
+      this.app.use(express.json());
+      this.app.use(express.urlencoded({ extended: true }));
+   }
+   private initRoutes() {
+      this.app.use("api/v1/users", userRoute);
+   }
+   private initErrorHandling() {
+      this.app.use(ErrorHandler.NotFound);
+      this.app.use(ErrorHandler.serverError);
+   }
+   public listen() {
+      this.app.listen(this.port, () => {
+         console.log(`Server running at localhost:${this.port}`);
+      });
+   }
 }
 
-export default App
+export default App;
